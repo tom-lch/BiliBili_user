75d02f62cd5e53f6289cff2213a30885aacdab12
wowowoll <49295186+wowowoll@users.noreply.github.com>
2019-10-16 13:49:40 +0800

    Update con.py

--- con.py
+++ con.py
@@ -1,7 +1,9 @@
 a = '__jp4({"code":0,"message":"0","ttl":1,"data":{"mid":16061199,"following":825,"whisper":0,"black":0,"follower":41635}})'
-
+#方法一：使用正则提取关注和粉丝量
 import re
 
 b = re.match('^f+', a)
 
 print(b)
+#方法二：
+b = a[6:-1]
